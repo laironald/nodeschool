67768732db99c8c92f0f68c5f360239c2cbf0e8c
Ron Lai <laironald@gmail.com>
2016-04-22 00:00:17 -0700

    more io

--- learnyounode/lines.js
+++ learnyounode/lines.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 
-var buf = fs.readFileSync(process.argv[2]);
-var lines = buf.toString().split('\n').length - 1;
+fs.readFile(process.argv[2], function(err, result) {
+  var lines = result.toString().split('\n').length - 1;
+  console.log(lines);
+});
 
-console.log(lines);
